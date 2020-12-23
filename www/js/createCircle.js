0d3849a6dbaf41511b9f84ff9a43004e5725a7fa
Vera de Lacombe <61185476+VeradeLacombe@users.noreply.github.com>
2020-12-23 18:31:35 +0100

    Add text input

--- www/js/createCircle.js
+++ www/js/createCircle.js
@@ -11,19 +11,19 @@ function radioClick() {
 	}
 	
 	if ($("#connectButton").is(":checked")) {
-		$("#explainConnectorChitchat").html("I want to answer statements about serious topics to connect on a deeper level");
+		$("#explainConnectorChitchat").html("I want to answer statements about serious topics<br>to connect on a deeper level");
 	}
 	if ($("#chitchatButton").is(":checked")) {
 		$("#explainConnectorChitchat").html("I want to answer lighthearted statements to connect in a fun way");
 	}
 	
 	if ($("#audioButton").is(":checked")) {
-		$("#explainCommunicationstyle").html("I want to connect via ONLY audiocalling");
+		$("#explainCommunicationstyle").html("I want to connect via text messaging and audiocalling");
 	}
 	if ($("#textmessagesButton").is(":checked")) {
-		$("#explainCommunicationstyle").html("I want to connect via ONLY text messaging");
+		$("#explainCommunicationstyle").html("I want to connect via text messaging");
 	}
 	if ($("#videoButton").is(":checked")) {
-		$("#explainCommunicationstyle").html("I want to connect via videocalling");
+		$("#explainCommunicationstyle").html("I want to connect via text messaging and videocalling");
 	}
 }
