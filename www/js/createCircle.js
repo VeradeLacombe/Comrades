bb10e9bed638a5b8f8e72f5c7b9bb889754f204c
Vera de Lacombe <61185476+VeradeLacombe@users.noreply.github.com>
2020-12-24 13:44:55 +0100

    Radio button input

--- www/js/createCircle.js
+++ www/js/createCircle.js
@@ -1,29 +1,42 @@
-$(document).ready(function() {
-	
-});
-
 function radioClick() {
-	if ($("#openButton").is(":checked")) {
-		$("#explainCloseorOpen").html("I want to meet new people and form a new community");
-	}
-	if ($("#closeButton").is(":checked")) {
-		$("#explainCloseorOpen").html("I want talk to my friends or my family!");
-	}
+	updateRadioButton("#openButton", "#explainCloseorOpen", "I want to meet new people and form a new community");
 	
-	if ($("#connectButton").is(":checked")) {
-		$("#explainConnectorChitchat").html("I want to answer statements about serious topics<br>to connect on a deeper level");
-	}
-	if ($("#chitchatButton").is(":checked")) {
-		$("#explainConnectorChitchat").html("I want to answer lighthearted statements to connect in a fun way");
-	}
+	updateRadioButton("#closeButton", "#explainCloseorOpen", "I want talk to my friends or my family!");
 	
-	if ($("#audioButton").is(":checked")) {
-		$("#explainCommunicationstyle").html("I want to connect via text messaging and audiocalling");
+	updateRadioButton("#connectButton", "#explainConnectorChitchat", "I want to answer statements about serious topics<br>to connect on a deeper level");
+	
+	updateRadioButton("#chitchatButton", "#explainConnectorChitchat", "I want to answer lighthearted statements to connect in a fun way");
+	
+	updateRadioButton("#audioButton", "#explainCommunicationstyle", "I want to connect via text messaging and audiocalling");
+	
+	updateRadioButton("#textmessagesButton", "#explainCommunicationstyle", "I want to connect via text messaging");
+	
+	updateRadioButton("#videoButton", "#explainCommunicationstyle", "I want to connect via text messaging and videocalling");
+}
+
+
+function updateRadioButton(buttonID, infoDivID, infoText) {
+	var button = $(buttonID);
+	if (button.is(":checked")) {
+		$(infoDivID).html(infoText);
+		button.parent().parent().find("label").css("border-style", "none");
 	}
-	if ($("#textmessagesButton").is(":checked")) {
-		$("#explainCommunicationstyle").html("I want to connect via text messaging");
+}
+
+function createCircle() {
+	var openOrClose = getRadioInput("open or close");
+	var connectOrChitchat = getRadioInput("connect or chitchat");
+	var audioTextOrVideo = getRadioInput("audio, text or video");
+	
+	if (openOrClose != undefined && connectOrChitchat != undefined && audioTextOrVideo != undefined) {
+		console.log({openOrClose, connectOrChitchat, audioTextOrVideo});
 	}
-	if ($("#videoButton").is(":checked")) {
-		$("#explainCommunicationstyle").html("I want to connect via text messaging and videocalling");
+}
+
+function getRadioInput(radioName) {
+	var value = $('input[name = "' + radioName + '"]:checked').val();
+	if (value == undefined) {
+		$('input[name = "' + radioName + '"]').parent().parent().find("label").css("border-style", "solid");
 	}
+	return value;
 }
